302680ec6ec0ed0024ea1c731859dae5e9a341f4
proninp <velninpapro@gmail.com>
2025-10-28 21:05:36 +0300

    Modeling the Books Domain

---
+++ project/RiverBooks/RiverBooks.Books/Book.cs
@@ -0,0 +1,27 @@
+﻿using Ardalis.GuardClauses;
+
+namespace RiverBooks.Books;
+
+internal sealed class Book
+{
+    public Guid Id { get; private set; } = Guid.NewGuid();
+
+    public string Title { get; private set; } = string.Empty;
+
+    public string Author { get; private set; } =  string.Empty;
+
+    public decimal Price { get; set; }
+
+    public Book(Guid id, string title, string author, decimal price)
+    {
+        Id = Guard.Against.Default(id);
+        Title = Guard.Against.NullOrEmpty(title);
+        Author = Guard.Against.NullOrEmpty(author);
+        Price = Guard.Against.Negative(price);
+    }
+
+    internal void UpdatePrice(decimal newPrice)
+    {
+        Price = Guard.Against.Negative(newPrice);
+    }
+}
