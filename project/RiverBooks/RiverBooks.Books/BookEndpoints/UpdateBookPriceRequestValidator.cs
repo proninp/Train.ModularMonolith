82ae128a8311c3ddde08dc07b30250578671ce11
proninp <velninpapro@gmail.com>
2025-11-02 16:45:36 +0300

    Added UpdateBookPriceRequest validator

---
+++ project/RiverBooks/RiverBooks.Books/BookEndpoints/UpdateBookPriceRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FastEndpoints;
+using FluentValidation;
+
+namespace RiverBooks.Books.BookEndpoints;
+
+public class UpdateBookPriceRequestValidator : Validator<UpdateBookPriceRequest>
+{
+    public UpdateBookPriceRequestValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotNull()
+            .NotEqual(Guid.Empty)
+            .WithMessage("Invalid Book Id.");
+        
+        RuleFor(x => x.NewPrice)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("Book Price must be greater than or equal to 0.");
+    }
+}
